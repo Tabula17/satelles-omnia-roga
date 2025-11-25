f45e561b7129808d7c3ea1c0c6591668df6d4418
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-25 18:22:38 -0300

    Introduce `Server` class to manage Swoole server configuration and dependencies

---
+++ src/Database/Server.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tabula17\Satelles\Omnia\Roga\Database;
+
+use Psr\Log\LoggerInterface;
+use Tabula17\Satelles\Omnia\Roga\LoaderInterface;
+
+class Server extends \Swoole\Server
+{
+
+    public ?LoggerInterface $logger;
+    public LoaderInterface $loader;
+    public PoolCollection $poolCollection;
+    public Connector $connector;
+
+    public function __construct(
+        Connector $connector,
+        PoolCollection $poolCollection,
+        LoaderInterface $loader,
+        string $host = '0.0.0.0',
+        int $port = 0,
+        int $mode = SWOOLE_BASE,
+        int $sock_type = SWOOLE_SOCK_TCP,
+        ?LoggerInterface $logger = null
+
+    )
+    {
+        $this->connector = $connector;
+        $this->poolCollection = $poolCollection;
+        $this->loader = $loader;
+        $this->logger = $logger;
+        parent::__construct($host, $port, $mode, $sock_type);
+    }
+}
