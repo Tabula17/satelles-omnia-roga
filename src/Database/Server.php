c058826a812a51e44c7e70770d1be596e11cda5d
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-26 09:28:42 -0300

    - Introduced `start()` method in `Server` class to handle logging, connection initialization, and error reporting. - Adjusted argument alignment in `Server` constructor for better readability. - Added usage of `InvalidArgumentException`.

--- src/Database/Server.php
+++ src/Database/Server.php
@@ -4,6 +4,7 @@ namespace Tabula17\Satelles\Omnia\Roga\Database;
 
 use Psr\Log\LoggerInterface;
 use Tabula17\Satelles\Omnia\Roga\LoaderInterface;
+use Tabula17\Satelles\Utilis\Exception\InvalidArgumentException;
 
 class Server extends \Swoole\Server
 {
@@ -14,14 +15,14 @@ class Server extends \Swoole\Server
     public Connector $connector;
 
     public function __construct(
-        Connector $connector,
+        Connector                  $connector,
         ConnectionConfigCollection $poolCollection,
-        LoaderInterface $loader,
-        string $host = '0.0.0.0',
-        int $port = 0,
-        int $mode = SWOOLE_BASE,
-        int $sock_type = SWOOLE_SOCK_TCP,
-        ?LoggerInterface $logger = null
+        LoaderInterface            $loader,
+        string                     $host = '0.0.0.0',
+        int                        $port = 0,
+        int                        $mode = SWOOLE_BASE,
+        int                        $sock_type = SWOOLE_SOCK_TCP,
+        ?LoggerInterface           $logger = null
 
     )
     {
@@ -30,5 +31,34 @@ class Server extends \Swoole\Server
         $this->loader = $loader;
         $this->logger = $logger;
         parent::__construct($host, $port, $mode, $sock_type);
+    }
+
+    /**
+     * Starts the TCP server and initializes connections.
+     *
+     * This method performs the necessary logging and loads connections
+     * into the pool collection. It processes the pool groups and logs
+     * their readiness status. If there are any unreachable connections,
+     * they are logged along with any associated errors.
+     *
+     * @return bool Returns true if the parent start method execution is successful.k
+     * @throws InvalidArgumentException
+     */
+    public function start(): bool
+    {
+        $this->logger->info("Iniciando servidor TCP en {$this->host}:{$this->port}");
+        $this->connector->loadConnections($this->poolCollection);
+        foreach ($this->connector->getPoolGroupNames() as $poolName) {
+            $this->logger->info("$poolName READY: " . $this->connector->getPoolCount($poolName) . ' pools >> ' . implode(', ', $this->connector->getPoolNamesForGroup($poolName)));
+        }
+        if ($this->connector->getUnreachableConnections()->count() > 0) {
+            $this->logger->error("Unreachable connections: " . implode(', ', $this->connector->getUnreachableConnections()->collect('name')));
+            foreach ($this->connector->getUnreachableConnections() as $conn) {
+                if (isset($conn->lastConnectionError)) {
+                    $this->logger->error($conn->name . ' -> ' . $conn->lastConnectionError);
+                }
+            }
+        }
+        return parent::start();
     }
 }
