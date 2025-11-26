0fd3dce2940369baa85315e3b4d2a2a1f43e69d2
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-26 12:16:45 -0300

    Refactor `DbConfigCollection` to extend `ConnectionCollection`, remove unused methods.

--- src/Database/DbConfigCollection.php
+++ src/Database/DbConfigCollection.php
@@ -3,33 +3,17 @@
 namespace Tabula17\Satelles\Omnia\Roga\Database;
 
 use Tabula17\Satelles\Omnia\Roga\Exception\InvalidArgumentException;
+use Tabula17\Satelles\Utilis\Collection\ConnectionCollection;
 use Tabula17\Satelles\Utilis\Collection\GenericCollection;
 
-class DbConfigCollection extends GenericCollection
+class DbConfigCollection extends ConnectionCollection
 {
-    protected static string $type = DbConfig::class;
+    public static string $type = DbConfig::class;
 
     public function __construct(DbConfig ...$config)
     {
         $this->values = $config;
-    }
-
-    /**
-     * @param string $name
-     * @return DbConfig|null
-     */
-    public function findByName(string $name): DbConfig|null
-    {
-        return $this->findBy('name', $name);
-    }
-    public function removeByName(string $name): void
-    {
-        $this->removeBy('name', $name);
-    }
-
-    public function filterByHost(string $name): DbConfigCollection
-    {
-        return $this->filterBy('host', $name);
+        parent::__construct();
     }
 
     /**
@@ -57,10 +41,7 @@ class DbConfigCollection extends GenericCollection
     {
         return $this->filter(fn(DbConfig $config) => $config->$key === $value);
     }
-    public function removeBy(string $key, mixed $value): void
-    {
-         $this->remove($this->findBy($key, $value));
-    }
+
     public static function fromArray(array $config): DbConfigCollection
     {
         return new self(...array_map(static fn($item) => $item instanceof self::$type ? $item : new self::$type($item), $config));
